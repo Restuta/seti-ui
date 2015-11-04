a05aee165cfa0760b82f843766a8cf252ea57ad6
Restuta <restuta8@gmail.com>
2015-11-03 20:29:55 -0800

    fixed directories to copy

--- copy-changes.sh
+++ copy-changes.sh
@@ -3,15 +3,12 @@ s#!/bin/bash
 #copies theme files from atom package folder
 ATOM_PACKAGES_DIR="/Users/Restuta/.atom/packages/seti-ui"
 
-SAMPLE_FILES_DIR="$ATOM_PACKAGES_DIR/sample-files"
-STYLES_DIR="$ATOM_PACKAGES_DIR/styles"
-INDEX_LESS_FILE="$ATOM_PACKAGES_DIR/index.less"
-
 DESTINATION="."
 
-cp -rv $SAMPLE_FILES_DIR $DESTINATION
-cp -rv $STYLES_DIR $DESTINATION
-cp -rv $INDEX_LESS_FILE $DESTINATION
+cp -rv "$ATOM_PACKAGES_DIR/styles" $DESTINATION
+cp -rv "$ATOM_PACKAGES_DIR/index.less" $DESTINATION
+cp -rv "$ATOM_PACKAGES_DIR/file-types" $DESTINATION
+
 
 echo "Done"
  
--- deploy-changes.sh
+++ deploy-changes.sh
@@ -3,15 +3,13 @@
 #copies theme files from atom package folder
 ATOM_PACKAGES_DIR="/Users/Restuta/.atom/packages/seti-ui"
 
-SAMPLE_FILES_DIR="./sample-files"
-STYLES_DIR="./styles"
-INDEX_LESS_FILE="./index.less"
+DESTINATION="$ATOM_PACKAGES_DIR"
 
-DESTINATION="$ATOM_PACKAGES_DIR/"
+cp -rv "./styles" $DESTINATION
+cp -rv "./index.less" $DESTINATION
+cp -rv "./file-types" $DESTINATION
 
-cp -rv $SAMPLE_FILES_DIR $DESTINATION
-cp -rv $STYLES_DIR $DESTINATION
-cp -rv $INDEX_LESS_FILE $DESTINATION
 
 echo "Done"
+
  
